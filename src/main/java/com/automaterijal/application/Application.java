e6aff5e6799efc3bd0d44cf8684c26c0df6ccc7f
Rade Spasojevic <radespasoje@gmail.com>
2019-06-12 12:14:17 +0200

    Adjusted app and logging route for tomcat

--- src/main/java/com/automaterijal/application/Application.java
+++ src/main/java/com/automaterijal/application/Application.java
@@ -1,15 +1,15 @@
 package com.automaterijal.application;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
     // You need to extend SpringBootServletInitializer for tomcat server
 
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+//    public static void main(final String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
 }
