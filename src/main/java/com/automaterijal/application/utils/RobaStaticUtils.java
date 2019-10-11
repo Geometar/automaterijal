f7cf794880de68b2f3ffcf3e33fedf3716bb541f
Rade Spasojevic <radespasoje@gmail.com>
2019-10-11 11:30:32 +0200

    Roba search fixed and expanded

--- src/main/java/com/automaterijal/application/utils/RobaStaticUtils.java
+++ src/main/java/com/automaterijal/application/utils/RobaStaticUtils.java
@@ -37,18 +37,24 @@ public class RobaStaticUtils {
         final Set<String> kataloskiBrojevi = new HashSet<>();
         if (!katBr.isEmpty()) {
             katBr.forEach(roba -> {
-                if(!StringUtils.isEmpty(roba.getKatbr()) && !" ".equals(roba.getKatbr())) {
-                kataloskiBrojevi.add(roba.getKatbr());
+                if (!StringUtils.isEmpty(roba.getKatbr()) && !" ".equals(roba.getKatbr())) {
+                    kataloskiBrojevi.add(roba.getKatbr());
                 }
 
-                if(!StringUtils.isEmpty(roba.getKatbrpro()) && !" ".equals(roba.getKatbrpro())) {
+                if (!StringUtils.isEmpty(roba.getKatbrpro()) && !" ".equals(roba.getKatbrpro())) {
                     kataloskiBrojevi.add(roba.getKatbrpro());
                 }
             });
         }
         if (!katBrProLista.isEmpty()) {
             katBrProLista.forEach(robaKatBrPro -> {
-                kataloskiBrojevi.add(robaKatBrPro.getKatbrpro());
+                if (!StringUtils.isEmpty(robaKatBrPro.getKatbr()) && !" ".equals(robaKatBrPro.getKatbr())) {
+                    kataloskiBrojevi.add(robaKatBrPro.getKatbr());
+                }
+
+                if (!StringUtils.isEmpty(robaKatBrPro.getKatbrpro()) && !" ".equals(robaKatBrPro.getKatbrpro())) {
+                    kataloskiBrojevi.add(robaKatBrPro.getKatbrpro());
+                }
             });
         }
         return new ArrayList(kataloskiBrojevi);
@@ -56,34 +62,34 @@ public class RobaStaticUtils {
 
     public String[] pronadjiSveVrsteUlja(final String vrstaUlja) {
         final String[] grupa = new String[2];
-        if(VrstaUlja.MOTORNA.getFieldName().equals(vrstaUlja)) {
+        if (VrstaUlja.MOTORNA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.MOTORNA_ULJA_SHELL;
             grupa[1] = GrupeKonstante.MOTORNA_ULJA_OSTALO;
-        } else if(VrstaUlja.MENJACKA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.MENJACKA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.MENJACKA_ULJA_SHELL;
             grupa[1] = GrupeKonstante.MENJACKA_ULJA_OSTALO;
-        } else if(VrstaUlja.KOCIONA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.KOCIONA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.KOCIONO_ULJA_SHELL;
             grupa[1] = GrupeKonstante.KOCIONO_ULJA_OSTALO;
-        } else if(VrstaUlja.ANTIFRIZ.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.ANTIFRIZ.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.ANTIFRIZ;
-        } else if(VrstaUlja.HIDRAULICNA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.HIDRAULICNA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.HIDRAULICNO_ULJA_SHELL;
             grupa[1] = GrupeKonstante.HIDRAULICNO_ULJA_OSTALO;
-        } else if(VrstaUlja.KOMPRESORSKA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.KOMPRESORSKA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.KOMPRESORSKA_ULJA_SHELL;
             grupa[1] = GrupeKonstante.KOMPRESORSKA_ULJA_OSTALO;
-        } else if(VrstaUlja.REDUTKORSKA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.REDUTKORSKA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.REDUTKORSKA_ULJA;
-        } else if(VrstaUlja.TRANSFORMATORSKA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.TRANSFORMATORSKA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.TRANSFORMATORSKA_ULJA;
-        } else if(VrstaUlja.TURBINSKA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.TURBINSKA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.TURBINSKA_ULJA;
-        } else if(VrstaUlja.PNEUMATSKA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.PNEUMATSKA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.PNEUMATSKA_ULJA;
-        } else if(VrstaUlja.KLIZNA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.KLIZNA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.KLIZNA_ULJA;
-        } else if(VrstaUlja.PREONOSNA.getFieldName().equals(vrstaUlja)){
+        } else if (VrstaUlja.PREONOSNA.getFieldName().equals(vrstaUlja)) {
             grupa[0] = GrupeKonstante.PREONOSNA_ULJA;
         }
         return grupa;
