56e38f0871dbd0647367b826b35eb70a53a9c59d
Rade Spasojevic <radespasoje@gmail.com>
2020-03-11 07:56:56 +0100

    Zavrsio logiku i stil za velike ekrane detalja robe

--- src/main/ui/src/app/e-shop/magacin/roba-detalji/roba-detalji.component.ts
+++ src/main/ui/src/app/e-shop/magacin/roba-detalji/roba-detalji.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RobaService } from '../../service/roba.service';
 import { Params, ActivatedRoute, Router } from '@angular/router';
-import { Roba } from '../../model/dto';
+import { Roba, RobaBrojevi } from '../../model/dto';
 import { takeWhile, finalize, catchError } from 'rxjs/operators';
 import { throwError, EMPTY } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
@@ -11,6 +11,7 @@ import { AppUtilsService } from '../../utils/app-utils.service';
 import { NotifikacijaService } from 'src/app/shared/service/notifikacija.service';
 import { MatSnackBarKlase } from 'src/app/shared/model/konstante';
 import { Location } from '@angular/common';
+import { Korpa } from 'src/app/e-shop/model/porudzbenica';
 
 @Component({
   selector: 'app-roba-detalji',
@@ -21,7 +22,9 @@ export class RobaDetaljiComponent implements OnInit {
 
   public robaDetalji: Roba;
   public kljuceviAplikacija: string[] = [];
+  public originalniBrojevi: OeBrojevi[];
 
+  private korpa: Korpa;
   public ucitavanje = false;
   public partnerLogovan = false;
   private alive = true;
@@ -38,6 +41,7 @@ export class RobaDetaljiComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.dataService.trenutnaKorpa.subscribe(korpa => this.korpa = korpa);
     this.loginServis.daLiJePartnerUlogovan.subscribe(bool => this.partnerLogovan = bool);
     this.uzmiDetaljeRobe();
   }
@@ -60,6 +64,7 @@ export class RobaDetaljiComponent implements OnInit {
         this.robaDetalji = res.body;
         this.robaDetalji = this.dataService.skiniSaStanjaUkolikoJeUKorpi([this.robaDetalji])[0];
         this.popuniAplikacije();
+        this.popuniOeBrojeve();
         console.log(this.robaDetalji);
       });
     });
@@ -103,12 +108,38 @@ export class RobaDetaljiComponent implements OnInit {
       console.log(this.kljuceviAplikacija);
     }
   }
+
+  popuniOeBrojeve() {
+    if (this.robaDetalji.tdBrojevi.length) {
+      this.originalniBrojevi = [];
+      this.robaDetalji.tdBrojevi.forEach((brojevi: RobaBrojevi) => {
+        if (!this.originalniBrojevi.some(oeBrojevi => brojevi.fabrBroj === oeBrojevi.broj)) {
+          this.originalniBrojevi.push(new OeBrojevi(brojevi.fabrBroj, brojevi.proizvodjac));
+        }
+      });
+    }
+  }
+
   vratiModelePoAutomobilu(automobil: string) {
     const a = this.robaDetalji.aplikacije[automobil];
     console.log(a);
     return this.robaDetalji.aplikacije[automobil];
   }
+
   idiNazad() {
     this.location.back();
+  }
+
+  uKorpi(katBr: string): boolean {
+    return this.utilsService.daLiJeRobaUKorpi(this.korpa, katBr);
+  }
+
+}
+class OeBrojevi {
+  broj: string;
+  proizvodjac: string;
+  constructor(broj, proizvodjac) {
+    this.broj = broj;
+    this.proizvodjac = proizvodjac;
   }
 }
