2abd6cc67ad9451214a46e7efc84756e8024259f
Rade Spasojevic <rspasojevic@inwerken.de>
2023-02-13 13:33:50 +0100

    Popravljen bug za logout

--- src/main/java/com/automaterijal/application/configuration/SecurityConfiguration.java
+++ src/main/java/com/automaterijal/application/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.automaterijal.application.configuration;
 import com.automaterijal.application.services.security.AuthEntryPointJwt;
 import com.automaterijal.application.services.security.AuthTokenFilter;
 import com.automaterijal.application.utils.LoginStaticUtils;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,121 +22,129 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableScheduling
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+  @Autowired
+  private UserDetailsService userDetailsService;
 
-    @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
+  @Autowired
+  private AuthEntryPointJwt unauthorizedHandler;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-        auth.authenticationProvider(authenticationProvider());
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService);
+    auth.authenticationProvider(authenticationProvider());
+  }
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
-    }
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    super.configure(web);
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-        http.sessionManagement()
-                .maximumSessions(10)
-                .sessionRegistry(sessionRegistry());
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable();
+    http.sessionManagement()
+        .maximumSessions(10)
+        .sessionRegistry(sessionRegistry());
 
-        http.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
-                .antMatchers("/api/auth/signin").permitAll()
-                .antMatchers("/static/**", "/").permitAll()
-                .antMatchers(
-                        "/api/roba/**", "/api/tecdoc/**", "/api/auth/signin", "/api/auth/**", "/api/informacije/**", "/api/admin/**", "/api/proizvodjaci/**", "/api/kategorije", "/api/kategorije/**", "/api/email/**",
-                        "/api/partner/promena-sifre", "/api/partner/read", "/api/partner/update", "/api/dashboard/**", "/api/izvestaj", "/api/izvestaj/firme").permitAll()
-                .antMatchers("/naslovna", "/api/log", "/api/fakture", "/o-nama", "/kontakt", "/roba", "/filteri", "/ulja", "/akumulatori", "/ostalo", "/ostalo/**", "/sitemap.xml", "/login**").permitAll()
-                .antMatchers("/login", "/reset-sifre/**").permitAll()
-                .antMatchers("/api/ExternalAccess/**").permitAll()
-                .antMatchers("/resources/**").permitAll()
-                .antMatchers("/*.js").permitAll()
-                .antMatchers("/*.ico").permitAll()
-                .antMatchers("/*.css").permitAll()
-                .antMatchers("/assets/slike/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
-                .permitAll();
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+    http.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .authorizeRequests()
+        .antMatchers("/api/auth/signin").permitAll()
+        .antMatchers("/static/**", "/").permitAll()
+        .antMatchers(
+            "/api/roba/**", "/api/tecdoc/**", "/api/auth/signin", "/api/auth/**",
+            "/api/informacije/**", "/api/admin/**", "/api/proizvodjaci/**", "/api/kategorije",
+            "/api/kategorije/**", "/api/email/**",
+            "/api/partner/promena-sifre", "/api/partner/read", "/api/partner/update",
+            "/api/dashboard/**", "/api/izvestaj", "/api/izvestaj/firme").permitAll()
+        .antMatchers("/naslovna", "/api/log", "/api/fakture", "/o-nama", "/kontakt", "/roba",
+            "/filteri", "/ulja", "/akumulatori", "/ostalo", "/ostalo/**", "/sitemap.xml",
+            "/login**").permitAll()
+        .antMatchers("/login", "/reset-sifre/**").permitAll()
+        .antMatchers("/api/ExternalAccess/**").permitAll()
+        .antMatchers("/resources/**").permitAll()
+        .antMatchers("/*.js").permitAll()
+        .antMatchers("/*.ico").permitAll()
+        .antMatchers("/*.css").permitAll()
+        .antMatchers("/assets/slike/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/api/roba")
+        .deleteCookies("JSESSIONID")
+        .invalidateHttpSession(true)
+        .permitAll();
 
-    @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter() {
-        return new AuthTokenFilter();
-    }
+    http.addFilterBefore(authenticationJwtTokenFilter(),
+        UsernamePasswordAuthenticationFilter.class);
+  }
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider
-                = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
+  @Bean
+  public AuthTokenFilter authenticationJwtTokenFilter() {
+    return new AuthTokenFilter();
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return LoginStaticUtils.md5Password(rawPassword.toString());
-            }
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider
+        = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
 
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return LoginStaticUtils.md5Password(rawPassword.toString()).equals(encodedPassword);
-            }
-        };
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new PasswordEncoder() {
+      @Override
+      public String encode(CharSequence rawPassword) {
+        return LoginStaticUtils.md5Password(rawPassword.toString());
+      }
 
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }
+      @Override
+      public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return LoginStaticUtils.md5Password(rawPassword.toString()).equals(encodedPassword);
+      }
+    };
+  }
 
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher() {
-        return new HttpSessionEventPublisher();
-    }
+  @Bean
+  public SessionRegistry sessionRegistry() {
+    return new SessionRegistryImpl();
+  }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-        configuration.setAllowedHeaders(List.of("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+  @Bean
+  public HttpSessionEventPublisher httpSessionEventPublisher() {
+    return new HttpSessionEventPublisher();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+    configuration.setAllowedHeaders(
+        List.of("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 }
